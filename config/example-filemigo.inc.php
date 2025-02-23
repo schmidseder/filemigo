5a3d84afb442ebb61342508e08c27a9658e2fb1d
Christian Schmidseder <c.schmidseder@gmx.de>
2025-02-23 14:59:49 +0100

    FMG_DATA_ROOT_BRANCHES added

--- config/example-filemigo.inc.php
+++ config/example-filemigo.inc.php
@@ -4,5 +4,11 @@
 return [
     'FMG_TITLE' => 'This are the files of the none existing person Josef Xaver Kreitmeier!',
     'FMG_FOOTER' => 'Josef Xaver Kreitmeier, Altstadt, Landshut, Niederbayern',
-    'FMG_DATA_ROOT' => DIR_DOCUMENT_ROOT . '/filemigo-data'
+    'FMG_DATA_ROOT' => DIR_DOCUMENT_ROOT . '/filemigo-data',
+    'FMG_DATA_ROOT_BRANCHES' => [
+        'directory_one',
+        'directory_two',
+        'directory_three',
+        'file_four.pdf'
+    ]
 ];
