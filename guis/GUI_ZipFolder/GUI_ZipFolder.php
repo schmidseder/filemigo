c1bf58b90b05e7c3146ba453fa2f210e6a940984
Christian Schmidseder <c.schmidseder@gmx.de>
2025-03-27 22:53:20 +0100

    zip archive generation

--- guis/GUI_ZipFolder/GUI_ZipFolder.php
+++ guis/GUI_ZipFolder/GUI_ZipFolder.php
@@ -7,6 +7,11 @@ use pool\classes\Core\Input\Filter\DataType;
 use pool\classes\Core\Input\Input;
 use pool\classes\GUI\GUI_Module;
 
+use RecursiveIteratorIterator;
+use RecursiveDirectoryIterator;
+use FilesystemIterator;
+use ZipArchive;
+
 class GUI_ZipFolder extends GUI_Module
 {
     /**
@@ -41,10 +46,51 @@ class GUI_ZipFolder extends GUI_Module
     {
         $success = false;
 
+
+
+        $index = $this->Session->getVar('index');
+
+        if ($index[$path]) {
+
+            $this->zipFolder($path, 'zip');
+        }
+
         return [
             'success' => $success,
             'path' => $path
         ];
+    }
+
+    private function zipFolder(string $path, string $target) : bool
+    {
+        $root = $this->Weblication->getConfigValue('FMG_DATA_ROOT');
+        $folder = $root . $path;
+
+        $files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($folder, FilesystemIterator::SKIP_DOTS));
+        $zip = new ZipArchive;
+        $create = $target . '.zip';
+
+        $index = $this->Session->getVar('index');
+
+        if ($zip->open($create, ZipArchive::CREATE)) {
+            foreach ($files as $file) {
+                $filepath = $this->removePrefix($file->getPathname(), $root);
+                if (isset($index[$filepath])) {
+                    $zip->addFile(realpath($file), $file);
+                }
+            }
+            $zip->close();
+        }
+
+        return file_exists($create);
+    }
+
+    private function removePrefix($fullString, $prefix) : string
+    {
+        if (str_starts_with($fullString, $prefix)) {
+            return substr($fullString, strlen($prefix)); // Entfernt den Prefix
+        }
+        return $fullString; // Falls der Prefix nicht gefunden wird, bleibt der String unverändert
     }
 }
 
