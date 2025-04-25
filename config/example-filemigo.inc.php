1f2dea18d28e34b520da1926c865ae0e81834132
Christian Schmidseder <c.schmidseder@gmx.de>
2025-04-25 15:01:59 +0200

    comments

--- config/example-filemigo.inc.php
+++ config/example-filemigo.inc.php
@@ -12,8 +12,12 @@ return [
     ],
     */
     'FMG_USERS' => [
-
+        /* ************************************************************************************** */
+        /* Please add the line containing the user and the password hash here.                    */
+        /* IMPORTANT: For security reasons, make sure to remove the line for the user "filemigo". */
         'filemigo' => '$2y$10$Voq267TbDd47AdxZcx4Ifuuga4LYRKc58S/IwN6hUcLrs2TSuLa3a',
+
+        /* ************************************************************************************** */
     ],
     'FMG_DATA_ROOT' => getenv('filemigo_data'),
     'FMG_ZIP_DIR'   => getenv('filemigo_zip')
